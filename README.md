f43723ce8c5030ca2c7773cedf8a38109b9d8084
PierreMarion23 <pierre.marion@polytechnique.edu>
2017-11-11 15:51:39 +0100

    Clean code + readme.

--- README.md
+++ README.md
@@ -1,23 +1,46 @@
-# sample-bot-botbuilder-qna
+# DB Request for Humans
 Sample bot using Microsoft BotBuilder
+
+## Main principles
+
+This bot uses the Microsoft Bot Framework. It is meant for users that want to request databases without any technical knowledge. It provides a user-friendly interface where the user can enter command-line-styled instructions for the bot. The main interest compared to a pure command line software is its flexibility, since it allows users to make small mistakes, and its customizability (the language used by the user to communicate with the bot can be very easily modified).
+
+On a technical standpoint, the bot frontend is mainly written in Node.js. The API calls are written in JS ES6, then transpiled using Babel. The backend is a Python Flask app. For now, the backend is fake, it simulates how a true backend would analyse the user's sentences and build a reply.
 
 ## To install and setup
 
 ### Install it
+
+To install the frontend:
+
 ```
 npm install
 ```
 
+To transpile JS ES6:
+
+```
+npm run build
+```
+
+For the backend, you can for example run it in a conda env. You need the following packages : Flask, Flask-RESTful, Flask-Cors.
 
 ## To run
 
-To run it, you need a server (your bot, you can see the implementation in [app.js](./app.js)) and a chat interface.
+To run it, you need to run the frontend (you can see the implementation in [app.js](./app.js)), the backend and a chat interface.
 
-### The bot
+### Frontend
 
 Run it with:
 ```
 node app.js
+```
+
+### Backend
+
+Run it with:
+```
+python run.py
 ```
 
 ### The test interface
@@ -30,3 +53,45 @@ You can use [Microsoft Bot Framework channel emulator](https://github.com/Micros
 
 You should now be able to talk to your bot on this interface. 
 
+## Examples of request
+
+The following prompts are keywords for the bot:
+
+```
+// to review the current state of your request:
+%review
+// to go back one step in your request:
+%back
+// to enter the help menu:
+%help
+// to learn some info about the DB structure:
+%explore
+// to send your request for treatment:
+%send
+// to cancel your request:
+%cancel
+```
+
+To test the bot's functionalities, you can enter the following prompts, which simulate a typical workflow:
+
+```
+Hello
+%explore
+toto tata titi
+foo baa
+%review
+%back
+foo bar
+%review
+%send
+```
+
+## Word of Thanks
+
+To [oscar6echo](https://github.com/oscar6echo), from whom originated the idea of this bot, and whose suggestions and help contributed to this project.
+
+To the people at [Botfuel](https://github.com/Botfuel) for their warm welcome and their help on this project.
+
+## Tips about Microsoft Bot Framework
+
+See [here](./tips.md).
--- app.js
+++ app.js
@@ -1,54 +1,19 @@
+var builder = require('botbuilder');
 const restify = require('restify');
-const builder = require('botbuilder');
-const QnA = require('botfuel-qna-sdk');
-const { Spellchecking } = require('botfuel-nlp-sdk');
+var api = require('./client_js/dist/api')
 const {
-  isGreetings,
-  isGoodbye,
-  BOT_DID_NOT_UNDERSTAND_MESSAGE,
-  BOT_SAY_HI,
-  BOT_SAY_GOODBYE,
-  BOT_ASK_FOR_SELECT_ACTION,
-  BOT_RECALL_USER_QUESTION,
-  DEFAULT_MESSAGE,
-} = require('./messages');
-
-// Botfuel App credentials
-const appId = '58432984';
-const appKey = '82d562ae661a2a91f3c6e230b810753d';
-
-const spellchecker = new Spellchecking({ appId, appKey });
-const QnAClient = new QnA({ appId, appKey });
-
-/**
- * Answer the user message using Botfuel QnA service
- * 
- * @param {Object} session the BotBuilder session
- * @param {String} sentence the user sentence
- * @callback callback a callback function, if specific action is required for bot answering
- */
-const answerWithQnA = (session, sentence, callback) => {
-  spellchecker.compute({ sentence, key: 'FR_1' }).then(response => {
-    QnAClient.getMatchingQnas({
-      sentence: response.correctSentence,
-    }).then(response => {
-      let message;
-      if (response.length === 1) {
-        // If only one intent is returned, answer with it.
-        message = response[0].answer;
-      } else if (response.length > 1) {
-        // If multiple intents are returned, enter `ambiguousDialog` dialog
-        session.replaceDialog('ambiguousQuestion', response);
-      } else {
-        // Else, send the default message.
-        message = DEFAULT_MESSAGE;
-      }
-      if (!(response.length > 1)) {
-        callback ? callback(message) : session.send(message);
-      }
-    });
-  });
-};
+    WELCOME_MESSAGE,
+    RETRY_PROMPT,
+    WAIT_PROMPT,
+    ASK_FOR_REQUEST_SPECIFICATION,
+    ASK_AGAIN_FOR_REQUEST_SPECIFICATION,
+    ASK_FOR_REQUEST_FROM_BACK_DIALOG,
+    CONFIRM_PROMPT,
+    END_OF_CONVERSATION,
+    READY_FOR_SENDING,
+    READY_FOR_SENDING_AGAIN,
+    ERROR_PROMPT
+  } = require('./messages');
 
 // Create a server
 const server = restify.createServer();
@@ -63,50 +28,316 @@ const connector = new builder.ChatConnector({
   // can be left undefined when testing with emulator
   appPassword: process.env.MICROSOFT_APP_PASSWORD,
 });
-const bot = new builder.UniversalBot(connector);
 
 // Create route /api/messages to communicate with your chat interface
 server.post('/api/messages', connector.listen());
 
-// Dialogs
-//////////
+// Setup bot and root waterfall
+var bot = new builder.UniversalBot(connector, [
+    function (session) {
+        // welcome message
+        session.send(WELCOME_MESSAGE);
+        session.conversationData.prompts = [];
+        session.conversationData.history_states_of_request = [[{}, false]];
+        // session.conversationData.requestIsReady = false;
+        session.beginDialog('main');
+    }
+]);
 
-bot.dialog('/', session => {
-  // Root dialog, says hi and goodbye, or ask QnA service
-  console.log(session.message);
-  if (isGreetings(session.message.text)) {
-    session.send(BOT_SAY_HI);
-  } else if (isGoodbye(session.message.text)) {
-    session.send(BOT_SAY_GOODBYE);
-  } else {
-    answerWithQnA(session, session.message.text);
-  }
-});
+bot.dialog('main', [
+    function(session, args){
+        // asks user for necessary information before being able to send request
+        session.beginDialog('specifyRequest', args);
+    },
+    function (session, results) {
+        // says request is ready, but user can still precise his request
+        session.beginDialog('readyForSending');
+    }   
+]);
 
-bot.dialog('ambiguousQuestion', (session, response) => {
-  // `ambiguousQuestion` dialog, to use when multiple intents are returned.
-  // Use BotBuilder postBack to address the issue.
-  if (response) {
-    // In this case, the dialog was called by root dialog: we expect actions to be displayed
-    const msg = new builder.Message(session)
-      .text(BOT_ASK_FOR_SELECT_ACTION)
-      .suggestedActions(
-        builder.SuggestedActions.create(
-          session,
-          response.map(answer =>
-            builder.CardAction.postBack(
-              session,
-              `${BOT_RECALL_USER_QUESTION} "${answer
-                .questions[0]}"\n\n -- \n\n${answer.answer}`,
-              answer.questions[0]
-            )
-          )
+bot.dialog('specifyRequest', [
+    function (session, args) {
+        console.log('beg of specify request')
+        console.log(args)
+        if (args && args.origin == 'self') {
+            builder.Prompts.text(session, ASK_AGAIN_FOR_REQUEST_SPECIFICATION,{retryPrompt: RETRY_PROMPT});
+        } else if (args && args.origin == 'backDialog'){
+            builder.Prompts.text(session, ASK_FOR_REQUEST_FROM_BACK_DIALOG,{retryPrompt: RETRY_PROMPT});
+        }
+        else {
+            builder.Prompts.text(session, ASK_FOR_REQUEST_SPECIFICATION, {retryPrompt: RETRY_PROMPT});
+        }
+    },
+    function(session, results, next){
+        console.log('make the user wait')
+        var msg = new builder.Message(session)
+        .text(WAIT_PROMPT)
+        .inputHint(builder.InputHint.ignoringInput);    // does not have any effect in the emulator
+        session.send(msg)
+        session.sendBatch() // to send message immediately
+
+        // simulate long request
+        setTimeout(function(){next(results);}, 5000)
+        // next(results)
+    },
+    function (session, results) {   
+        console.log('end of specify request')
+        sendToSpecify(session, results).then(
+            function(ready){
+                console.log(ready)
+                if (ready){     // move to next dialog in waterfall
+                    session.endDialog()        
+                }
+                else {          // replay the same dialog
+                    session.replaceDialog('specifyRequest', { origin: 'self' })
+                }
+            }
         )
-      );
-    session.send(msg);
-  } else {
-    // In this case, the dialog was called by itself: we expect selected question to be answered.
-    session.send(session.message.text);
-    session.endConversation();
-  }
+    }
+]).beginDialogAction('reviewAction', 'reviewRequest', {
+    matches: /^%review$/i
+}).beginDialogAction('backAction', 'backDialog', {
+    matches: /^%back/i
+})// Once triggered, will end the conversation.
+.endConversationAction('endConversationAction', END_OF_CONVERSATION, {
+    matches: /^%cancel$/i,
+    confirmPrompt: CONFIRM_PROMPT
 });
+
+bot.dialog('readyForSending', [
+    function (session, args) {
+        console.log('args')
+        console.log(args)
+        if (args && args.origin == 'self') {
+            builder.Prompts.text(session, READY_FOR_SENDING_AGAIN, {retryPrompt: RETRY_PROMPT});
+        } 
+        else if (args && args.origin == 'request_sent'){
+            builder.Prompts.text(session, RETRY_PROMPT, {retryPrompt: RETRY_PROMPT});            
+        }
+        else {
+            builder.Prompts.text(session, READY_FOR_SENDING, {retryPrompt: RETRY_PROMPT});
+        }
+    },
+    function (session, results) {
+        sendToSpecify(session, results).then(
+            function(ready){
+                console.log(ready)
+                if (ready){
+                    session.replaceDialog('readyForSending', { origin: 'self' });
+                }
+                else {
+                    session.replaceDialog('main', { origin: 'ready_for_sending' });
+                }
+            }
+        )
+    }
+]).beginDialogAction('reviewAction', 'reviewRequest', {
+    matches: /^%review$/i
+}).beginDialogAction('sendAction', 'requestSent', {
+    matches: /^%send/i
+}).beginDialogAction('backAction', 'backDialog', {
+    matches: /^%back/i
+}).endConversationAction('endConversationAction', END_OF_CONVERSATION, {
+    matches: /^%cancel$/i,
+    confirmPrompt: CONFIRM_PROMPT   // ask the user for confirmation before erasing the conversation
+});
+
+bot.dialog('requestSent', function (session, args, next) {
+    //Send the request to backend for treatment
+    var msg = new builder.Message(session)
+    .text(WAIT_PROMPT)
+    .inputHint(builder.InputHint.ignoringInput);    // does not have any effect in the emulator
+    session.send(msg)
+    session.sendBatch() // to send message immediately
+
+    return new Promise(function(resolve, reject) {
+        console.log('in promise of requestSent')
+        array = session.conversationData.history_states_of_request
+        current_request = array[array.length - 1][0]
+        api.default.Post('/compute', {'sentence':session.message.text, 'state_of_request':current_request},
+        function(response){
+            console.log('response')
+            console.log(response.data)
+            if (! response.data.request_successful){
+                session.send(ERROR_PROMPT)
+                reject('error') 
+            }
+            response.data.sentences.forEach(function(sentence){
+                session.send(sentence);
+            });
+            console.log("Returning to parent dialog...")
+            session.replaceDialog('readyForSending', {origin:'request_sent'});
+            resolve()
+        }, 
+        function(error){
+            console.log('error')
+            session.send(ERROR_PROMPT)
+            console.log("Returning to parent dialog...")
+            session.replaceDialog('readyForSending', {origin:'request_sent'});
+            reject('error')
+        })   
+    })
+})
+
+bot.dialog('reviewRequest', function (session, args, next) {
+    // Review the request current state
+    session.send('Reviewing request...');
+    array = session.conversationData.history_states_of_request
+    current_request = array[array.length - 1][0]
+    console.log(current_request)
+    var keys = Object.keys(current_request);
+    keys.forEach(function(key){
+        session.send('column ' + key + ' with value ' + current_request[key]);
+    });
+    console.log("Returning to parent dialog...");
+    session.endDialog();
+})
+
+bot.dialog('backDialog', [function (session, args, next) {
+    // Back up to the previous state of request
+    session.send('Backing one step up...');
+    array = session.conversationData.history_states_of_request
+    if (array.length > 1){
+        array.pop()
+    }
+    else{
+        session.send('You already went back to initial state, cannot go back anymore.')
+    }
+    next()
+}, function (session, args, next){
+    session.beginDialog('reviewRequest');
+}, function(session, args){
+    ready = array[array.length-1][1]
+    if (ready){
+        session.replaceDialog('readyForSending', {origin:'backDialog'})
+    }
+    else {
+        session.replaceDialog('main', {origin:'backDialog'})
+    }
+}]);
+
+
+bot.dialog('explore', 
+    function (session, args, next) {
+        session.send('Exploring mode');
+
+        return new Promise(function(resolve, reject) {
+            console.log('in promise of exploring')
+            api.default.Post('/exploration', {'sentence':session.message.text},
+            function(response){
+                console.log('response')
+                console.log(response.data)
+                if (! response.data.request_successful){
+                    session.send(ERROR_PROMPT)
+                    reject('error') 
+                }
+                response.data.sentences.forEach(function(sentence){
+                    session.send(sentence);
+                });
+                console.log("Returning to parent dialog...")
+                session.endDialog()
+                resolve()
+            }, 
+            function(error){
+                console.log('error')
+                session.send(ERROR_PROMPT)
+                console.log("Returning to parent dialog...")
+                session.endDialog()
+                reject('error')
+            })   
+        })
+    })
+// Once triggered, will start a new dialog as specified by
+// the 'onSelectAction' option.
+.triggerAction({
+    matches: /^%explore/i,
+    onSelectAction: (session, args, next) => {
+        // Add the explore dialog to the top of the dialog stack 
+        // (override the default behavior of replacing the stack)
+        session.beginDialog(args.action, args)
+    }
+});
+
+// Example help dialog, to indicate a possible structure.
+bot.dialog('help', [
+    function (session, args, next) {
+        session.send('This is an interactive help menu');
+        builder.Prompts.choice(session, "What do you want to know about:", 'General Bot description|Link to github detailed doc|Examples');
+    },
+    function (session, results) {
+        session.dialogData.path = results.response.index;
+        builder.Prompts.choice(session, "Which level of verbosity:", 'Low|High');
+    },
+    function (session, results) {
+        verbose = results.response.index
+        switch (session.dialogData.path) {
+            case 0:
+                if (verbose){
+                    session.send('Here is the very verbose bot description')
+                }
+                else{
+                    session.send('Here is the general bot description')
+                }        
+                break;
+            case 1:
+                session.send('Here are the link to github');
+                break;
+            case 2:
+                session.send('Here are some examples');
+                break;
+        }
+        builder.Prompts.choice(session, "Exit help?", 'Yes|No');
+    },
+    function (session, results) {
+        decision = results.response.index;
+        if (decision == 1){
+            session.replaceDialog('help')
+        }
+        else{
+            session.endDialog()
+        }
+    }
+])
+// Once triggered, will start a new dialog as specified by
+// the 'onSelectAction' option.
+.triggerAction({
+    matches: /^%help$/i,
+    onSelectAction: (session, args, next) => {
+        // Add the help dialog to the top of the dialog stack 
+        // (override the default behavior of replacing the stack)
+        session.beginDialog(args.action, args);
+    }
+})
+// Once triggered, will end the dialog. 
+.cancelAction('cancelHelpAction', 'Leaving help mode, returning to normal mode...', {
+    matches: /^%cancel$/i
+});
+
+
+
+var sendToSpecify = function (session, results) {
+    // analyse user's request here.
+    session.conversationData.prompts.push(results.response)
+    console.log(results.response)
+    return new Promise(function(resolve, reject) {
+        console.log('in promise of sendToSpecify')
+        array = session.conversationData.history_states_of_request
+        current_request = array[array.length - 1][0]
+        api.default.Post('/specify', {'sentence':session.message.text, 'state_of_request':current_request},
+        function(response){
+            console.log('response of /specify')
+            console.log(response.data)
+            array.push([response.data.state_of_request, response.data.ready]);
+            response.data.sentences.forEach(function(sentence){
+                session.send(sentence);
+                });
+            console.log('after backend /specify response')
+            resolve(response.data.ready)
+        }, 
+        function(error){
+            console.log('error backend')
+            session.send(ERROR_PROMPT)
+            reject('error')
+        })
+    });
+}
